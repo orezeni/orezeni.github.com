0aa37ebceeff5f38105ba7e50e3588030d9c1506
hagino3000 <hagino.3000@gmail.com>
2012-07-16 20:16:59 +0900

    Fixed #7 横浜データでのBubble Treeの実装

---
+++ js/bubbletree.cofog.js
@@ -0,0 +1,46 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+
+var BubbleTree = BubbleTree || {};
+BubbleTree.Styles = BubbleTree.Styles || {};
+
+BubbleTree.Styles.Cofog1 = {
+
+	/* yokohama cofog */
+	'01': { icon: 'icons/helping-others.svg', color: '#935B3B' },
+	'02': { icon: 'icons/schools.svg', color: '#0AB971' },
+	'03': { icon: 'icons/civilian-action.svg', color: '#EC2406' },
+	'04': { icon: 'icons/order-safety.svg', color: '#790586' },
+	'05': { icon: 'icons/environment.svg', color: '#2A3A03' },
+	'06': { icon: 'icons/garbage.svg', color: '#D33673' },
+	'07': { icon: 'icons/our-streets.svg', color: '#D33673' },
+	'08': { icon: 'icons/economy-tourism.svg', color: '#4E6D00' },
+	'09': { icon: 'icons/government.svg', color: '#C75746' },
+	'10': { icon: 'icons/traffic-watersup.svg', color: '#938626' }
+}
+
+BubbleTree.Styles.Cofog2 = {
+
+	'01.1': { icon: 'icons/helping-others.svg'},
+	'02.1': { icon: 'icons/pre-school.svg'},
+	'02.2': { icon: 'icons/education.svg'},
+	'03.1': { icon: 'icons/civilian-action.svg'},
+	'04.1': { icon: 'icons/order-safety.svg'},
+	'05.1': { icon: 'icons/wind.svg'},
+	'05.2': { icon: 'icons/tree.svg'},
+	'06.1': { icon: 'icons/garbage.svg'},
+	'07.1': { icon: 'icons/housing.svg'},
+	'07.2': { icon: 'icons/street-lights.svg'},
+	'07.3': { icon: 'icons/farms.svg'},
+	'08.1': { icon: 'icons/culture.svg'},
+	'08.2': { icon: 'icons/dollar.svg'},
+	'08.3': { icon: 'icons/harbor.svg'},
+	'09.1': { icon: 'icons/publicaffairs.svg'},
+	'09.2': { icon: 'icons/government.svg'},
+	'10.1': { icon: 'icons/traffic-watersup.svg'}
+};
+
+BubbleTree.Styles.Cofog3 = {
+}
