69a04289a8759f29bc72ca1751f5e383260c2796
Hal Seki <hal@plants-web.jp>
2013-03-15 00:26:28 +0900

    modified

--- js/setting.js
+++ js/setting.js
@@ -2,8 +2,17 @@
  * Icons for COFOG taxonomy
  *
  */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'yokohama_budget';
+OpenSpending.year = '2012';
+
 OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
@@ -38,11 +47,4 @@ OpenSpending.Styles.Cofog = {
   '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
 };
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
 
-var Taxes = Taxes || {};
-
-Taxes.baseKoujo = 330000; // 住民税基礎控除
-Taxes.huyoKoujo = 330000; // 一人分の扶養控除
-Taxes.taxRate = 0.06; // 住民税率
