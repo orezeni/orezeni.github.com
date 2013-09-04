d42b3c75fe9c3bad7e01f28c81cd7bb97e6a58ed
WithMachida <withmachida@gmail.com>
2013-09-04 13:32:35 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'machidagen';
-OpenSpending.year = '2013';
+OpenSpending.identifier = 'yokohama_budget';
+OpenSpending.year = '2012';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
