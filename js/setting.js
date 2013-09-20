ff3ddc9368946415461b25ba065f0774c1b34a7e
WithMachida <withmachida@gmail.com>
2013-09-20 16:39:25 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -52,7 +52,7 @@ OpenSpending.Styles.Cofog = {
   '2': { icon: 'icons/machi.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '3': { icon: 'c_kenkou_fukushi.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3': { icon: 'icons/c_kenkou_fukushi.svg', color: '#C75746', bcolor: '#4E6D00' },
   '3-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#4E6D00' },
   '3-2': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#4E6D00' },
   '4': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#D33673' },
