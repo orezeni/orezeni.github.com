38bf495c768439fe43e0b02c192b89ee6cd5663f
ats777 <ats777@gmail.com>
2014-02-06 20:18:45 +0900

    changed taxRate from 0.06 to 0.04 in js/setting.js

--- js/setting.js
+++ js/setting.js
@@ -8,7 +8,7 @@ var Taxes = Taxes || {};
 
 Taxes.baseKoujo = 330000; // 住民税基礎控除
 Taxes.huyoKoujo = 330000; // 一人分の扶養控除
-Taxes.taxRate = 0.06; // 住民税率
+Taxes.taxRate = 0.04; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
