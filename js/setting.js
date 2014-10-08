604b21c085fc64d32081c866ce16d1dc9ef35aef
Jizai2012ikusei <Jinza2012ikusei@gmail.com>
2014-10-08 14:53:01 +0900

    openspending.identifie
    hai

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
+OpenSpending.identifier = 'jinzai2012ikusei';
 OpenSpending.year = '2012';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
