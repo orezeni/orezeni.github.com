6fbefbc52e077c3bc3b513daedb138f68d706d08
Jizai2012ikusei <jinzai2012ikusei@gmail.com>
2014-10-08 16:16:31 +0900

    year2014

--- js/setting.js
+++ js/setting.js
@@ -11,7 +11,7 @@ Taxes.taxRate = 0.06; // 住民税率
 var OpenSpending = OpenSpending || {};
 
 OpenSpending.identifier = 'jinzai2012ikusei';
-OpenSpending.year = '2012';
+OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
