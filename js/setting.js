ccc86c81f424ef8b5f73cb886ed99c1ba1654aa1
wordsystem <kiyohara.wordsystem@gmail.com>
2014-10-09 18:11:31 +0900

    cc

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'tsuyama-yosan2014';
+OpenSpending.identifier = 'Tsuyama-Yosan2014';
 OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
