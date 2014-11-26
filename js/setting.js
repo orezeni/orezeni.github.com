42da0763cda9b9a6fa61d6f83feaae902cd0423e
Jizai2012ikusei <jinzai2012ikusei@gmail.com>
2014-11-26 15:45:06 +0900

    year2014
    q

--- js/setting.js
+++ js/setting.js
@@ -18,8 +18,8 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '100': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '100-101': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
   '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
   '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
