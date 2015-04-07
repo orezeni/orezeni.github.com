3f8fd8e41593d9674b7324b58d649f0df893d222
Jizai2012ikusei <jinzai2012ikusei@gmail.com>
2015-04-07 12:59:25 +0900

    setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'jinzai2012ikusei';
+OpenSpending.identifier = 'jinzai2017ikusei';
 OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
@@ -19,12 +19,12 @@ OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
   '1000': { icon: 'icons/planning.svg', color: '#00bfff', bcolor: '#00bfff' },
-  '1000-1100': { icon: 'icons/planning.svg', color: '#00bfff', bcolor: '#00bfff' },
+  '1100': { icon: 'icons/planning.svg', color: '#00bfff', bcolor: '#00bfff' },
   '2000': { icon: 'icons/family2.svg', color: '#00bfff', bcolor: '#c757ff' },
   '2100': { icon: 'icons/publicaffairs.svg', color: '#00bfff', bcolor: '#0AB971' },
-  '2100-2101': { icon: 'icons/research.svg', color:'#00bfff' , bcolor:''  },
-  '2100-2102': { icon: 'icons/money.svg', color: '#00bfff', bcolor: '#4E6D00' },
-  '2100-2103': { icon: 'icons/human-resources.svg', color: '#00bfff', bcolor: '#D33673' },
+  '2101': { icon: 'icons/research.svg', color:'#00bfff' , bcolor:''  },
+  '2102': { icon: 'icons/money.svg', color: '#00bfff', bcolor: '#4E6D00' },
+  '2103': { icon: 'icons/human-resources.svg', color: '#00bfff', bcolor: '#D33673' },
   '2200': { icon: 'icons/family.svg', color: '#00bfff', bcolor: '#D33673' },
   '2300': { icon: 'icons/government.svg', color: '#00bfff', bcolor: '#2A3A03' },
   '3000': { icon: 'icons/unenployment.svg', color: '#00bfff', bcolor: '#2A3A03'  },
