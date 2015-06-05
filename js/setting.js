7081e9a10f836964a20fb8ddc078e034c2540ab6
Jizai2012ikusei <jinzai2012ikusei@gmail.com>
2015-06-05 10:50:54 +0900

    setting.js

--- js/setting.js
+++ js/setting.js
@@ -27,7 +27,7 @@ OpenSpending.Styles.Cofog = {
   '2103': { icon: 'icons/human-resources.svg', color: '#00bfff', bcolor: '#D33673' },
   '2200': { icon: 'icons/family.svg', color: '#00bfff', bcolor: '#D33673' },
   '2300': { icon: 'icons/government.svg', color: '#00bfff', bcolor: '#2A3A03' },
-  '3000': { icon: 'icons/unenployment.svg', color: '#00bfff', bcolor: '#2A3A03'  },
+  '3000': { icon: 'icons/unemployment.svg', color: '#00bfff', bcolor: '#2A3A03'  },
   '3100': { icon: 'icons/EC.svg', color: '#00bfff', bcolor: '#EC2406' },
   '3101': { icon: 'icons/euro.svg', color: '#00bfff', bcolor: '#EC2406' },
   '3102': { icon: 'icons/secondary-lower.svg', color: '#00bfff', bcolor: '#938626' },
